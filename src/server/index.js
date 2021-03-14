2ca58648c69bb03427dac98a577ffec79d1809a7
Emiri Begic <emiri.begic@gmail.com>
2021-03-14 20:21:34 +0100

    Add webpack

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = 8081;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-const port = 8081;
 app.listen(port, () => {
 	console.log(`Server running on localhost: ${port}`);
 });
