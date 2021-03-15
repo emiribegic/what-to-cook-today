37a63329b2e123fbce0e7472777caf259777f190
Emiri Begic <emiri.begic@gmail.com>
2021-03-15 19:20:52 +0100

    Add node-fetch

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8083;
+const port = process.env.PORT || 8083;
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 app.use(express.urlencoded({ extended: false }));
@@ -10,20 +11,5 @@ app.use(cors());
 app.use(express.static('dist'));
 
 app.get('/', (req, res) => res.sendFile('index.html', { root: '../../dist' }));
-
-const base = 'https://api.edamam.com/search?';
-const input = document.querySelector('.search__input').value;
-const apiId = process.env.API_ID;
-const apiKey = process.env.API_KEY;
-
-app.get('/recipe', async () => {
-	const res = await fetch(
-		`${base}q=${input}&app_id=${apiId}&app_key=${apiKey}`
-	);
-	const json = await res.json();
-
-	if (!res.ok) throw new Error(`${json.message}`);
-	console.log(res, data);
-});
 
 app.listen(port, () => console.log(`Server running on localhost: ${port}`));
