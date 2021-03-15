77dabf33429d80a08f1c224121080e554b8066c8
Emiri Begic <emiri.begic@gmail.com>
2021-03-15 12:23:07 +0100

    Add dotenv

---
+++ src/client/index.js
@@ -0,0 +1,1 @@
+import searchRecipe from './js/formHandler';
--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 8083;
+require('dotenv').config();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -9,5 +10,20 @@ app.use(cors());
 app.use(express.static('dist'));
 
 app.get('/', (req, res) => res.sendFile('index.html', { root: '../../dist' }));
+
+const base = 'https://api.edamam.com/search?';
+const input = document.querySelector('.search__input').value;
+const apiId = process.env.API_ID;
+const apiKey = process.env.API_KEY;
+
+app.get('/recipe', async () => {
+	const res = await fetch(
+		`${base}q=${input}&app_id=${apiId}&app_key=${apiKey}`
+	);
+	const json = await res.json();
+
+	if (!res.ok) throw new Error(`${json.message}`);
+	console.log(res, data);
+});
 
 app.listen(port, () => console.log(`Server running on localhost: ${port}`));
